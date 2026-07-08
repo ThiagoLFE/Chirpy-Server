948bc4cfc4ec3e35f3783a96ea083e72e1e80be5
ThiagoLFE <thiago.rib1905@gmail.com>
2026-07-07 21:30:42 -0300

    feat: monolith architecture start

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -12,14 +18,49 @@ func main() {
 		Handler: mux,
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("content-type", "text/plain; charset=utf-8")
-		w.WriteHeader(200)
+	cfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("GET /api/healthz", readinessServe)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerNumberRequests)
+	mux.HandleFunc("POST /admin/reset", cfg.handlerResetCountRequests)
+	mux.Handle("/app/", cfg.middlewareCountRequest(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
 	log.Printf("Server is running at http://localhost%s", server.Addr)
 	log.Fatal(server.ListenAndServe())
+}
+
+func (c *apiConfig) middlewareCountRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (c *apiConfig) handlerNumberRequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write(fmt.Appendf([]byte{}, `
+		<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>
+		`,
+		c.fileserverHits.Load(),
+	))
+}
+
+func readinessServe(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+
+	w.Write([]byte("OK"))
+}
+
+func (c *apiConfig) handlerResetCountRequests(w http.ResponseWriter, _ *http.Request) {
+	c.fileserverHits.Store(0)
+	w.Write(fmt.Appendf([]byte{}, "reseted hits"))
+
 }
